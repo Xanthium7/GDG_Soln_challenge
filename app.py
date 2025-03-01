8159927cfde6ef1c7315a20a1c70b0bf205c0069
Xanthium7 <akshhaykmurali@gmail.com>
2025-03-01 09:33:31 +0530

    Add initial prompt for elderly assistance and requirements in app.py; create requirements.txt

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from groq import Groq
 
 load_dotenv()
 
+PROMPT = "You are an expert assistant who hels elderly ppl with documents and procedures in lesgl sectors like pachayath village offices banks etc.. This is pirmarily specific to keralal. an Elderly person will ask yyou a prompt about some clasrification he would require to help them identify their various requirements. return your rezponse ina simple straight forward way"
+
 client = Groq(
     api_key=os.getenv("GROQ_API_KEY"),
 )
 
 chat_completion = client.chat.completions.create(
     messages=[
+        {"role": "system", "content": PROMPT},
         {
             "role": "user",
             "content": "Explain the importance of fast language models",
